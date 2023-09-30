467b36cc17be31ed2fe9537bb881bdff0c8e13c1
kaycee04 <kingsleyotuya04@gmail.com>
2023-09-30 09:58:00 +0100

    updated path

--- src/components/NavBar1.jsx
+++ src/components/NavBar1.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+// import {Nav, Container, Navbar, NavDropdown} from "react-bootstrap"
 
 const NavBar1 = () => {
+  const location = useLocation()
+
+  useEffect(()=>{
+    console.log(location.pathname);
+  }, [location.pathname])
+
+
   return (
-    <div className='d-flex justify-content-around py-4'>
-      <Link to='/'>Home</Link>
-      <Link to='task'>Tasks</Link>
-      <Link to='new'>New Task</Link>
-      {/* <Link to='edit'>Edit Task</Link> */}
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+    <Container>
+      <Link to="/">Task Duty</Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className='flex-lg-grow-0'>
+        <Nav className="me-auto">
+          {location.pathname === "/task" ? null : <Link to="/task">All Task</Link>}
+          {/* {location.pathname === "/new" ? null :<Link to="/new">New Task</Link>} */}
+          {location.pathname === "/new"  || location.pathname.includes("/edit/:id") ? null :<Link to="/new">New Task</Link>}
+          
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   )
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HomePhoto from '../assets/Images/Component 1.png'
+import { Link } from 'react-router-dom'
 
 const Homepage = () => {
   return (
@@ -8,7 +9,7 @@ const Homepage = () => {
         <h1 className='manage'>Manage your Tasks on</h1>
         <h1 className='task mx-0'>TaskDuty</h1>
         <p className='home-lorem align-content-start fs-4 my-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non tellus, sapien, morbi ante nunc euismod ac felis ac. Massa et, at platea tempus duis non eget. Hendrerit tortor fermentum bibendum mi nisl semper porttitor. Nec accumsan.</p>
-        <button className='home-btn rounded-3 py-2 px-5 fs-4'>Go to My Tasks</button>
+        <Link  to="/task" className='home-btn rounded-3 py-3 px-5 fs-4 text-decoration-none'>Go to My Tasks</Link>
       </div>
 
       <div>
